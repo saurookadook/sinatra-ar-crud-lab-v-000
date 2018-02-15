01fe6cb88c5032c36da643713e5ffa0afd5f427e
Andy Maskiell <maskiella@gmail.com>
2018-02-15 19:55:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
 class Post < ActiveRecord::Base
   # attr_accessor :name, :content
-  # 
+  #
   # ALL_POSTS = []
-  # 
+  #
   # # def initialize(info_hash)
   # #   @name = info_hash[:name]
   # #   @content = info_hash[:content]
   # #   ALL_POSTS << self
   # # end
-  # 
+  #
   # def initialize(name: nil, content: nil)
   #   @name = name
   #   @content = content
   #   ALL_POSTS << self
   # end
-  # 
+  #
   # def self.all
   #   ALL_POSTS
   # end
